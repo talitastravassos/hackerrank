12c18e1e6018539789fa726affd433f037b276c3
talitastravassos <talitatravassos.tt@gmail.com>
2018-11-21 17:35:06 -0300

    Day 13 to Day 15 of 30 Days of Code Challenge by HackerRank.

---
+++ 30 Days of Code/Day 15 - Linked List.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+class Node {
+	int data;
+	Node next;
+	Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+class Solution {
+
+    public static  Node insert(Node head,int data) {
+        //Complete this method
+        Node new_data = new Node(data);
+        
+        if(head == null){
+            head = new_data;
+            return head;
+        }
+        Node last = head;
+        while(true){
+            if(last.next == null){
+                last.next = new_data;
+                return head;
+            }
+            else{
+                last = last.next;
+            }
+        }
+        }
+
+	public static void display(Node head) {
+        Node start = head;
+        while(start != null) {
+            System.out.print(start.data + " ");
+            start = start.next;
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        Node head = null;
+        int N = sc.nextInt();
+
+        while(N-- > 0) {
+            int ele = sc.nextInt();
+            head = insert(head,ele);
+        }
+        display(head);
+        sc.close();
+    }
+}
